74e312a9062176a7af08728d9dde235056a30658
joshir <hareepjoshi@gmail.com>
2023-10-26 22:50:01 -0500

    test that backpressure from subscriber is respected by dummy publisher

--- lib/src/main/java/tiny/reactor/DummyPublisher.java
+++ lib/src/main/java/tiny/reactor/DummyPublisher.java
@@ -9,19 +9,28 @@ public class DummyPublisher<T> implements Publisher<T>{
 
   @Override
   public void subscribe(Subscriber<? super T> s) {
+    final int[] index = {0};
     s.onSubscribe(new Subscription() {
       @Override
       public void request(long n) {
+        for(int i = 0; i < n && index[0] < arr.length; i++, index[0]++){
+          T t = arr[index[0]];
+
+          if (t == null) {
+            s.onError(new NullPointerException("element must not be null"));
+            return;
+          }
+
+          s.onNext(t);
+        }
+
+        if(index[0] == arr.length)
+          s.onComplete();
       }
 
       @Override
       public void cancel() {
       }
     });
-
-    for(T e: arr)
-      s.onNext(e);
-
-    s.onComplete();
   }
 }
--- lib/src/test/java/tiny/reactor/DummyPublisherTest.java
+++ lib/src/test/java/tiny/reactor/DummyPublisherTest.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class DummyPublisherTest extends AbstractPublisherTest{
   @Test
@@ -39,5 +40,51 @@ public class DummyPublisherTest extends AbstractPublisherTest{
 
     Assertions.assertThat(latch.await(1000, TimeUnit.MICROSECONDS)).isTrue();
     Assertions.assertThat(sequence).containsExactly("onSubscribe() ok", "onNext(0) ok", "onNext(1) ok", "onNext(2) ok", "onComplete() ok");
+  }
+
+  @Test
+  public void publisherRespectsSubscriberBackpressure() throws InterruptedException {
+    DummyPublisher<Long> dummy = new DummyPublisher<>(generate(5L));
+    final Subscription[] subscription = new Subscription[1];
+    CountDownLatch latch = new CountDownLatch(1);
+    AtomicReference<Throwable> error = new AtomicReference<>();
+    List<Long> collected = new ArrayList<>();
+    dummy.subscribe(new Subscriber<>() {
+      @Override
+      public void onSubscribe(Subscription s) {
+        subscription[0] = s;
+      }
+
+      @Override
+      public void onNext(Long aLong) {
+        collected.add(aLong);
+      }
+
+      @Override
+      public void onError(Throwable t) {
+        error.set(t);
+      }
+
+      @Override
+      public void onComplete() {
+        latch.countDown();
+      }
+    });
+
+
+    // checks that backpressure from subscription is respected by publisher
+    Assertions.assertThat(collected).isEmpty();
+
+    subscription[0].request(1);
+    Assertions.assertThat(collected).containsExactly(0L);
+
+    subscription[0].request(1);
+    Assertions.assertThat(collected).containsExactly(0L, 1L);
+
+    subscription[0].request(2);
+    Assertions.assertThat(collected).containsExactly(0L, 1L, 2L, 3L);
+
+    subscription[0].request(Long.MAX_VALUE);
+    Assertions.assertThat(latch.await(1000, TimeUnit.MICROSECONDS)).isTrue();
   }
 }
